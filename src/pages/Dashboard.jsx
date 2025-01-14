6db213e0cd87d5502056d3579ccfe4b7f7be36fa
Shamim Al Mamun <shamim2601@gmail.com>
2025-01-15 01:40:54 +0600

    Update Dashboard.jsx share

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -254,7 +254,7 @@ const Dashboard = () => {
   const handleShare = (result) => {
     const shareData = {
       title: `Exam Performance: ${result.exam_details.title}`,
-      text: `I scored ${result.correct_count} out of ${result.exam_details.num_questions} in the ${result.exam_details.title} exam.`,
+      text: `Exam : ${result.exam_details.title} (Id-${result.exam_details.id})\n Score : ${result.correct_count} / ${result.exam_details.num_questions} in ${result.exam_duration} minutes `,
       url: window.location.href
     };
 
